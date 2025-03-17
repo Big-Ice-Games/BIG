56b0be6a01b39f16394ce6c73ffa1b110ac1cdd6
Hostur <f.zielinski.gliwice@gmail.com>
2025-03-17 10:54:27 +0100

    Added system args utils

---
+++ Runtime/Utils/Args.cs
@@ -0,0 +1,27 @@
+﻿namespace BIG.Utils
+{
+    public static class Args
+    {
+        /// <summary>
+        /// If <see cref="System.Environment.GetCommandLineArgs()"/> contains +connect_lobby argument this function will give you lobby id.
+        /// </summary>
+        /// <param name="lobbyId">Steam lobby id.</param>
+        /// <returns>Value indicating whether there is a lobby player should join.</returns>
+        public static bool GetSteamConnectionLobbyId(out ulong lobbyId)
+        {
+            lobbyId = 0;
+            string[] args = System.Environment.GetCommandLineArgs();
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] != "+connect_lobby" || i + 1 >= args.Length) continue;
+                if (ulong.TryParse(args[i + 1], out lobbyId))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
